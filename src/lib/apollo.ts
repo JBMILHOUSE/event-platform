e150915645631be384c3beb5d44b25914aae02fc
JBMILHOUSE <christopher.alexandre.oliver@gmail.com>
2022-06-26 22:09:33 -0300

    aula 04 e 05 finalizadas

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4tfkklc01u701ue99zic9wf/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authoriztion': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
